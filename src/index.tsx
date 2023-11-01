c17837e900237ddbfa142ab2a1e5ccc4ddb45c88
Neeraj A <n.attavar.c@gmail.com>
2023-11-01 19:56:52 +0530

    Add i18next for multi language support

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes'
 
+import './i18n'
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Suspense fallback='loading...'>
+      <RouterProvider router={router} />
+    </Suspense>
   </React.StrictMode>,
 )
